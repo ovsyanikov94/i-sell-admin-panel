afcfde8366869c3db472384e1b4a0c10fd0b611e
dikiy77 <dikiy_77@mail.ru>
2018-11-11 23:16:06 +0300

    add & get works

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -23,27 +23,145 @@ function GetClearMessage( validationError ){
 
 }//GetClearMessage
 
-module.exports.AddCategory = async( req , res )=>{
+module.exports = {
+    AddCategory: async (req, res) => {
 
-    try{
+        try {
 
-        let title = req.body.categoryTitle;
+            let title = req.body.categoryTitle;
 
-        let newCategory = new Category({
-            title: title
+            //console.log( title );
+
+            let newCategory = new Category({
+                title: title
+            });
+
+            let result = await newCategory.save();
+
+            //console.log( newCategory );
+
+            res.send({
+                'category': result
+            });
+
+        }//try
+        catch (ex) {
+
+            res.send(ex.message);
+
+        }//catch
+
+    },
+
+    categoriesList: async (req, res) => {
+
+    //console.log('LIST!');
+
+    try {
+
+        let categories = await Category.find().populate('lots', {
+            select: [
+                'id',
+                'lotTitle'
+            ]
         });
 
-        let result = await newCategory.save();
 
-        res.send( {
-            'category': result
-        } );
+        //console.log('categories' , categories);
+
+        res.send({
+            statusCode: 200,
+            data: categories,
+            message: 'Success!'
+        });
 
     }//try
-    catch(ex){
+    catch (ex) {
 
-        res.send( ex.message );
+        console.log(ex);
 
     }//catch
+
+
+},//categoriesList
+
+    updateCategory: async ( req , res )=>{
+
+        try{
+
+            let categoryID = req.params.id;
+
+            if(!await Category.findOne({id: categoryID})){
+                return res.send({
+                    code:400,
+                    message:'Категория с таким ID не найдена!',
+                    data:categoryID
+                });
+            }//if
+
+            let categoryTitle = req.body.categoryTitle;
+
+            if(categoryTitle.trim().length === 0){
+                return res.send({
+                    code:400,
+                    message:'некорректные данные',
+                    data:categoryTitle
+                });
+            }//if
+
+            let category = await Category.findOne({categoryTitle: newCategoryTitle});
+
+            if(category){
+                return res.send({
+                    code:400,
+                    message:'категория с таким названием уже существует!',
+                    data:category
+                });
+            }//if
+
+
+            let updatedCategory = await Category.update({id: categoryID} , {categoryTitle:categoryTitle}).fetch();
+
+            res.send(updatedCategory);
+
+        }//try
+        catch(ex){
+
+            res.send(ex);
+
+        }//catch
+
+    },//updatedCategory
+
+    deleteCategory: async ( req , res )=>{
+
+        try{
+
+            let categoryID = req.params.id;
+
+            if(!await Category.findById({categoryID})){
+                return res.send({
+                    code:400,
+                    message:'Категория с таким ID не найдена!',
+                    data:categoryID
+                });
+            }//if
+
+            let deletedCategory = await Category.remove({id: categoryID});
+            console.log( deletedCategory );
+            res.send({
+                code:200,
+                message:'Категория удалена!',
+                data:deletedCategory
+            });
+
+        }//try
+        catch(ex){
+
+            res.send(ex);
+
+        }//catch
+
+    },//deleteCategory
 
 };
