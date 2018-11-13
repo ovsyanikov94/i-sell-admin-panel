f9b5db013915da58c8916444649322d44b97b8e5
mrvet <alexoovsyanikov@gmail.com>
2018-11-13 12:40:07 +0300

    Gulp tasks was added

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const gulp = require('gulp');
+const gulpSync = require('gulp-sync')(gulp);
+
+const connection = require('./model/connection');
+
+//Очистка статусов сделок
+require('./tasks/clear-deal-statuses');
+
+//Вставка статусов сделок
+require('./tasks/insert-deal-statuses');
+
+gulp.task( 'default' , gulpSync.sync([
+    'clearDealStatuses',
+    'InsertDefaultDealStatuses'
+]));
