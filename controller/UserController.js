cf6783f4704cd876d544c8b9e470aff577df479e
mrvet <alexoovsyanikov@gmail.com>
2018-11-13 10:15:54 +0300

    Register/Update user fix

--- controller/UserController.js
+++ controller/UserController.js
@@ -19,9 +19,7 @@ module.exports.AddUser = async( req , res ) => {
     let validPassword = constValidator.USER_PASSWORD_VALIDATOR.test(req.body.password)||'';
     let validRole =validator.isMongoId(req.body.role)||'';
     let validUserStatus = validator.isMongoId(req.body.userStatus)||'';
-    //let rating = +req.body.rating;
-    // let lots = req.body.lots;
-    //let image = req.body.image;
+
     if(!validLogin||
         !validEmail||
         !validFirstName||
@@ -107,14 +105,17 @@ module.exports.AddUser = async( req , res ) => {
 }; // AddUser
 
 module.exports.updateUser = async(req,res)=>{
-    let validIdUser = validator.isMongoId(req.body.userId)||''
+
+    let validIdUser = validator.isMongoId(req.body.userId)||'';
 
     if(!validIdUser){
+
         Response.status = 400;
         Response.message = 'не корректное значени!';
-        res.status(Response.status)
+        res.status(Response.status);
         res.send(Response);
         return;
+
     }//if
     try {
 
@@ -123,133 +124,174 @@ module.exports.updateUser = async(req,res)=>{
         });
 
         if(!existUser){
+
             Response.status = 400;
             Response.message = 'не корректное значени!';
-            res.status(Response.status)
+            res.status(Response.status);
             res.send(Response);
             return;
+
         }//if
 
         let validEmail = constValidator.USER_EMAIL_VALIDATOR.test(req.body.email)||'';
+
         if(validEmail){
+
             existUser.set({
                 email:req.body.email
-            })
-        }
-        else{
-            Response.status = 400;
-            Response.message = 'не корректный email!';
-            res.status(Response.status)
-            res.send(Response);
-            return;
-        }
-        let validFirstName = constValidator.USER_FIRSTNAME_VALIDATOR.test(req.body.firstName)||'';
-        if(validFirstName){
-            existUser.set({
-                firstName:req.body.firstName
-            })
+            });
+
         }//if
         else{
+
+            Response.status = 400;
+            Response.message = 'не корректный email!';
+            res.status(Response.status);
+            res.send(Response);
+            return;
+
+        }//else
+
+        let validFirstName = constValidator.USER_FIRSTNAME_VALIDATOR.test(req.body.firstName)||'';
+
+        if(validFirstName){
+
+            existUser.set({
+                firstName:req.body.firstName
+            });
+
+        }//if
+        else{
+
             Response.status = 400;
             Response.message = 'не корректное имя !';
             res.status(Response.status)
             res.send(Response);
             return;
+
         }//else
+
         let validLastName = constValidator.USER_LASTNAME_VALIDATOR.test(req.body.lastName)||'';
+
         if(validLastName){
+
             existUser.set({
                 lastName:req.body.lastName
-            })
+            });
+
         }//if
         else{
+
             Response.status = 400;
             Response.message = 'не корректная фамилия !';
-            res.status(Response.status)
+            res.status(Response.status);
             res.send(Response);
             return;
+
         }//else
+
         let validPhone = constValidator.USER_PHONE_VALIDATOR.test(req.body.phone)||'';
+
         if(validPhone){
+
             existUser.set({
                 phone:req.body.phone
             })
+
         }//if
         else{
+
             Response.status = 400;
             Response.message = 'не корректный телефон !';
-            res.status(Response.status)
+            res.status(Response.status);
             res.send(Response);
             return;
+
         }//else
 
         let validRole =validator.isMongoId(req.body.role)||'';
+
         if(validRole){
+
             existUser.set({
                 role:req.body.role
             })
+
         }//if
         else{
+
             Response.status = 400;
             Response.message = 'не корректная роль !';
-            res.status(Response.status)
+            res.status(Response.status);
             res.send(Response);
             return;
+
         }//else
+
         let validUserStatus = validator.isMongoId(req.body.userStatus)||'';
-        if(validRole){
+
+        if(validUserStatus){
+
             existUser.set({
                 userStatus:req.body.userStatus
-            })
+            });
+
         }//if
         else{
+
             Response.status = 400;
             Response.message = 'не корректный статус !';
-            res.status(Response.status)
+            res.status(Response.status);
             res.send(Response);
             return;
+
         }//else
 
         let validPasswordOld = constValidator.USER_PASSWORD_VALIDATOR.test(req.body.oldPassword)||'';
         let validPasswordNew = constValidator.USER_PASSWORD_VALIDATOR.test(req.body.newPassword)||'';
-        if(validPasswordOld&&validPasswordNew) {
 
-            let oldHexPassword = bcrypt.hashSync(validPasswordOld,existUser.saltStr); // получаем закодированный пароль
+        if(validPasswordOld && validPasswordNew) {
 
-            if(existUser.password===oldHexPassword){
+            let compare = await bcrypt.compare(req.body.oldPassword ,existUser.password );
+
+            if(compare === true ){
 
                 let number = Math.floor(Math.random() * (19 - 9+1) ) + 5 //генерируем случайное число символов от 9 до 19
-                let saltStr = bcrypt.genSaltSync(number);// создаем соль
-                let newHexPassword = bcrypt.hashSync(req.body.newPassword, saltStr); // получаем закодированный пароль
+                let saltStr = await bcrypt.genSalt(number);// создаем соль
+                let newHexPassword = await bcrypt.hash(req.body.newPassword, saltStr); // получаем закодированный пароль
 
                 existUser.set({
-                    saltStr:saltStr,
                     password:newHexPassword
                 });
+
             }//if
             else {
+
                 Response.status = 400;
                 Response.message = 'не корректный пароль !';
-                res.status(Response.status)
+                res.status(Response.status);
                 res.send(Response);
                 return;
+
             }//else
+
         }//if
         else{
+
             Response.status = 400;
             Response.message = 'не корректный пароль !';
-            res.status(Response.status)
+            res.status(Response.status);
             res.send(Response);
             return;
+
         }//else
 
         let updateUser = await existUser.save();
 
         Response.status = 200;
         Response.message = 'обновления прошли успешно!';
-        Response.data = updateUser;
-        res.status(Response.status)
-        res.send(Response);
+        Response.data = null;
+
     }//try
     catch (ex){
 
@@ -265,8 +307,10 @@ module.exports.updateUser = async(req,res)=>{
         Response.status = 500;
         Response.message = 'Внутренняя ошибка сервера!';
         Response.data = null;
-        res.status(Response.status)
-        res.send(Response);
 
     }//catch
-}//updateUser
+
+    res.status(Response.status);
+    res.send(Response);
+
+};//updateUser
