1554088febbd2ec1f1521792c70af6a0b4098895
Jitman123 <the12gitman@gmail.com>
2018-11-14 16:28:49 +0300

    Comment commit

--- model/CommentStatus.js
+++ model/CommentStatus.js
@@ -2,11 +2,18 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const constValidator = require('./Validation');
 
 const commentStatusSchema = new Schema({
 
     commentStatusTitle: {
         type: String,
+        validate:{
+            validator:(commentStatusTitle)=>{
+                return constValidator.COMMENT_STATUS_AND_TYPE_VALIDATOR.test(commentStatusTitle)
+            },
+            message: props => `Название статуса комментария некорректно: "${props.value}"`
+        }
     },
 
 });
--- model/CommentType.js
+++ model/CommentType.js
@@ -2,11 +2,18 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const constValidator = require('./Validation');
 
 const commentTypeSchema = new Schema({
 
     commentTypeTitle: {
         type: String,
+        validate:{
+            validator:(commentTypeTitle)=>{
+                return constValidator.COMMENT_STATUS_AND_TYPE_VALIDATOR.test(commentTypeTitle)
+            },
+            message: props => `Название типа комментария некорректно: "${props.value}"`
+        }
     },
 
 });
