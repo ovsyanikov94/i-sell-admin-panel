d86fce99758ff344c8b7b73e3597405770201e40
YukaSan <mr.uakor@gmail.com>
2018-11-14 13:56:15 +0300

    UserAPI_List

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,16 @@ const connection = require('./model/connection');
 
 //Очистка статусов сделок
 require('./tasks/clear-deal-statuses');
-
 //Вставка статусов сделок
 require('./tasks/insert-deal-statuses');
+//очистка роли юзера
+require('./tasks/clear-user-role');
+//добавление роли юзера
+require('./tasks/insert-user-role')
+//очистка статуса юзера
+require('./tasks/clear-user-status');
+//добавление статуса юзера
+require('./tasks/insert-user-status')
 
 gulp.task( 'default' , gulpSync.sync([
     'clearDealStatuses',
